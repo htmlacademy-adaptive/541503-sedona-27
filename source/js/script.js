4e1266fa4c2115581bd151f92df52b60555e011f
Alexandr Pavlychenko <apavlychenko23@gmail.com>
2023-01-14 02:16:01 +0200

    Adds decor without images

---
+++ source/js/script.js
@@ -0,0 +1,13 @@
+﻿const mainNav = document.querySelector('.main-nav');
+
+mainNav.classList.remove('main-nav--open');
+
+const mainNavToggle = document.querySelector('.main-nav-toggle');
+
+mainNavToggle.onclick = function () {
+  if (mainNav.classList.contains('main-nav--open')) {
+    mainNav.classList.remove('main-nav--open');
+  } else {
+    mainNav.classList.add('main-nav--open');
+  }
+};
